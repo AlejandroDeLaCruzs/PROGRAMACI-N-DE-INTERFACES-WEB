c404f74404a828b1acda969eea2d784beb322039
AlejandroDeLaCruzs <alexcruzs1105@gmail.com>
2026-03-19 20:36:43 +0100

    style: carrito mas bonito

--- clase5/src/app/Components/carrito.tsx
+++ clase5/src/app/Components/carrito.tsx
@@ -7,13 +7,29 @@ type Params = {
   setMostrarCarrito: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export const Carrito = ({ children, productos, setLista, setMostrarCarrito }: Params) => {
+export const Carrito = ({
+  children,
+  productos,
+  setLista,
+  setMostrarCarrito,
+}: Params) => {
   return (
-    <div>
+    <div className="carritoCointener">
+      <div className="title">
+        <h1>{children}</h1>
         <button onClick={() => setMostrarCarrito(false)}>Cerrar</button>
-      <h1>{children}</h1>
-      {productos && productos.map((e) => 
-      <button onClick={() => setLista(productos.filter((x)=> x.id !== e.id ))}>{e.name}</button>)}
+      </div>
+      <div className="innerContiner">
+        {productos &&
+          productos.map((e) => (
+            <div key={e.id}>
+              <p className="elementoLista"
+                onClick={() => setLista(productos.filter((x) => x.id !== e.id))}
+              >{e.name}</p>
+              
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
--- clase5/src/app/page.tsx
+++ clase5/src/app/page.tsx
@@ -1,41 +1,50 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import ProductComponent from "./Components/product";
 import { Carrito } from "./Components/carrito";
 
 export type Producto = {
-  id: string,
-  name: string
-}
+  id: string;
+  name: string;
+};
 export const Home = () => {
   const lista = ["ordendor", "movil", "televisor", "auriculares"];
-  const [myCompra, setMyCompra] = useState<Producto[]>([]); 
+  const [myCompra, setMyCompra] = useState<Producto[]>([]);
   const [open, setOpen] = useState(false);
   const [showCarrito, setShowCarrito] = useState(false);
 
   console.log(myCompra);
-  
-  
+
   return (
     <div className="main">
-      <button onClick={()=> setShowCarrito(true)}>mostrar compra</button>
-      {showCarrito && <Carrito productos={myCompra} setLista={setMyCompra} setMostrarCarrito={setShowCarrito} >Carrito de compra</Carrito>}
+      {!showCarrito && (
+        <img
+          onClick={() => setShowCarrito(true)}
+          src={"5465865.png"}
+          className="carritoImg"
+        />
+      )}
+      {showCarrito && (
+        <Carrito
+          productos={myCompra}
+          setLista={setMyCompra}
+          setMostrarCarrito={setShowCarrito}
+        >
+          Carrito de compra
+        </Carrito>
+      )}
       <h1>Productos:</h1>
-      {lista.map((e) => <ProductComponent key={e} name={e} lista={myCompra} setLista={setMyCompra}/>)}
-      <div>
-        <h2>Productos comprados:</h2>
-        {myCompra.map((e) => (
-          <div key={e.id}>
-            <h3 onClick={() => {
-              setMyCompra(myCompra.filter((x) => x.id !== e.id))
-            }} className="elementoLista">{e.name}</h3>
-            </div>
-        ))}
-      </div>
+      {lista.map((e) => (
+        <ProductComponent
+          key={e}
+          name={e}
+          lista={myCompra}
+          setLista={setMyCompra}
+        />
+      ))}
     </div>
   );
 };
-
 
 export default Home;
