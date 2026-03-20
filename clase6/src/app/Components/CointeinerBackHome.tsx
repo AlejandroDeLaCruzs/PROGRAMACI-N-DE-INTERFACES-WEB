4ad7641a82d37e4bf6cf46582d5538ccdf83c03f
AlejandroDeLaCruzs <alexcruzs1105@gmail.com>
2026-03-20 13:03:58 +0100

    feat: fakestore api con herencia de componente

--- clase6/src/app/Components/CointeinerBackHome.tsx
+++ clase6/src/app/Components/CointeinerBackHome.tsx
@@ -1,16 +1,17 @@
-
 "use client"
 
 export const CointnerBackHome = ({
   children,
+  setMostrar
 }: {
   children: React.ReactNode;
+  setMostrar: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   return (
-    <div className="mainCointener">
+    <div onClick={(e) =>{ setMostrar(false); e.stopPropagation()}}>
       <div className="data">
         {children}
-        <h1>hola</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet necessitatibus unde expedita ipsum, et placeat laborum doloribus ea assumenda recusandae facere eligendi magni quibusdam velit! Quisquam rem ex voluptates reprehenderit!</p>
       </div>
     </div>
   );
--- clase6/src/app/page.tsx
+++ clase6/src/app/page.tsx
@@ -1,20 +1,19 @@
-
+"use client";
 import { useState } from "react";
 import { CointnerBackHome } from "./Components/CointeinerBackHome";
 
-
 export const Home = () => {
-  const [mostrar, setMostrar] = useState<boolean>(true);
-
-
+  const [mostrar, setMostrar] = useState<boolean>(false);
 
   return (
-    <div className="main">
-      <CointnerBackHome>
-        <h1>esto esta fuera del componente</h1>
-      </CointnerBackHome>
+    <div className="mainCointener" onClick={() => setMostrar(true)}>
+      {mostrar && (
+        <CointnerBackHome setMostrar={setMostrar}>
+          <h1>esto esta fuera del componente</h1>
+        </CointnerBackHome>
+      )}
     </div>
   );
-}
+};
 
 export default Home;
